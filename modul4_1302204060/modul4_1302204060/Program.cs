70576d3df51906702662a3681611bd52a014e6d3
epenuts <rizkyventa00@gmail.com>
2022-03-19 13:52:10 +0700

    menambahkan class Penjumlahan dengan methodJumlahTigaAngka

--- modul4_1302204060/modul4_1302204060/Program.cs
+++ modul4_1302204060/modul4_1302204060/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302204060
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // memnaggil generic method Penjumlahan
+            Console.WriteLine(Penjumlahan.JumlahTigaAngka<long>(13, 02, 20));
+        }
+    }
+}
+class Penjumlahan
+{
+    public static T JumlahTigaAngka<T>(T x, T y, T z)
+    {
+        dynamic val_x = x;
+        dynamic val_y = y;
+        dynamic val_z = z;
+        return val_x + val_y + val_z;
+    }
+}
