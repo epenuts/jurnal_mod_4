85afb108710c5dcfe747b208cc58d8c001fa9775
epenuts <rizkyventa00@gmail.com>
2022-03-19 14:08:34 +0700

    menambahkan class SimpleDataBase

--- modul4_1302204060/modul4_1302204060/Program.cs
+++ modul4_1302204060/modul4_1302204060/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302204060
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // validasi generic class SimpleDataBase
+            SimpleDataBase<long> db = new SimpleDataBase<long>();
+            db.AddNewData(0);
+            db.AddNewData(13);
+            db.AddNewData(02);
+            db.AddNewData(20);
+            db.PrintAllData();
+        }
+    }
+
+    class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            this.storedData = new List<T>();
+            this.inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T newData)
+        {
+            this.inputDates.Add(DateTime.Now);
+            this.storedData.Add(newData);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 1; i < this.inputDates.Count; i++)
+            {
+                Console.WriteLine("Data " + i + " berisi: " + this.storedData[i] +
+                    ", yang disimpan pada waktu UTC: " + this.inputDates[i]);
+            }
+        }
+    }
+}
